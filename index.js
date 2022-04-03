ebe42c86e448c2f5255ceb97c5f2031da3f12ed0
likeAkingCompany <kunasagbe.com>
2022-04-03 02:10:51 +0100

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 var favicon = require('serve-favicon');
-//const multer = require("multer");
-
+//const multer = require("multer")
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT ||3030 
